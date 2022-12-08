3e5f5130ce5756ec47497748009a2fd025679eca
MalikaBoss <malika.boss@kcl.ac.uk>
2022-12-08 20:20:41 +0000

    do exercise

--- frequencies.py
+++ frequencies.py
@@ -4,5 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    
+    for val in items:
+        if checkItem(val, frequencies):
+            frequencies[str(val)] = 1
+        else:
+            frequencies[str(val)] = frequencies[str(val)] + 1
     return frequencies
+
+def checkItem(item, frequencies):
+    if str(item) not in frequencies.keys():
+        return True
+    return False
